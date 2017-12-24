05acc400a339512bd7a98c394c893ab378c721f8
junlee91 <jlee757@uic.edu>
2017-12-24 01:05:57 -0600

    user followers/following list

--- sodagram/users/serializers.py
+++ sodagram/users/serializers.py
@@ -19,7 +19,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
             'images'
         )
 
-class ExploreUserSerializer(serializers.ModelSerializer):
+class ListUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.User
--- sodagram/users/urls.py
+++ sodagram/users/urls.py
@@ -23,4 +23,14 @@ urlpatterns = [
         view=views.UserProfile.as_view(),
         name='user_profile'
     ),
+    url(
+        regex=r'^(?P<username>\w+)/followers/$',
+        view=views.UserFollowers.as_view(),
+        name='user_followers'
+    ),
+    url(
+        regex=r'^(?P<username>\w+)/following/$',
+        view=views.UserFollowing.as_view(),
+        name='user_following'
+    ),
 ]
