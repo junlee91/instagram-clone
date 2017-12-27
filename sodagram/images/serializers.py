ac9696feffc033c2711a6d0aa0cc3feb30d6d0d4
junlee91 <jlee757@uic.edu>
2017-12-27 15:42:14 -0600

    edit image contents

--- sodagram/images/serializers.py
+++ sodagram/images/serializers.py
@@ -73,3 +73,13 @@ class ImageSerializer(serializers.ModelSerializer):
             'creator',
             'created_at'
         )
+
+class InputImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Image
+        fields = (
+            'file',
+            'location',
+            'caption',
+        )
--- sodagram/images/views.py
+++ sodagram/images/views.py
@@ -206,3 +206,24 @@ class ImageDetail(APIView):
         serializer = serializers.ImageSerializer(image)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    
+    def put(self, request, image_id, format=None):
+
+        user = request.user
+
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)     
+
+        serializer = serializers.InputImageSerializer(image, data=request.data, partial=True)
+
+        if serializer.is_valid():
+
+            serializer.save(creator=user)
+
+            return Response(data=serializer.data, status=status.HTTP_204_NO_CONTENT)
+
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
