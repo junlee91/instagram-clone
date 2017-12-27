6f1eb99cce511bb7e76e6717a877db9f973febed
junlee91 <jlee757@uic.edu>
2017-12-27 16:03:20 -0600

    Delete Image

--- sodagram/images/views.py
+++ sodagram/images/views.py
@@ -194,6 +194,13 @@ class ModerateComments(APIView):
 
 class ImageDetail(APIView):
 
+    def find_own_image(self, image_id, user):
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
+
     def get(self, request, image_id, format=None):
 
         user = request.user
@@ -212,10 +219,10 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
-        except models.Image.DoesNotExist:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)     
+        image = self.find_own_image(image_id, user) 
+
+        if image is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED) 
 
         serializer = serializers.InputImageSerializer(image, data=request.data, partial=True)
 
@@ -227,3 +234,17 @@ class ImageDetail(APIView):
 
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    def delete(self, request, image_id, format=None):
+
+        user = request.user
+
+        image = self.find_own_image(image_id, user) 
+
+        if image is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED) 
+
+        image.delete()
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
