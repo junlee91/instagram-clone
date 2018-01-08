e9cc1b5161571392a1341e1549fc291bb596b349
junlee91 <jlee757@uic.edu>
2018-01-08 17:12:21 -0600

    Controlling inputs and submit on SignupForm

--- frontend/src/components/SignupForm/container.js
+++ frontend/src/components/SignupForm/container.js
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import SignupForm from "./presenter";
 
-const Container = props => <SignupForm {...props}/>
+class Container extends Component {
+  state = {
+    email: "",
+    fullname: "",
+    username: "",
+    password: ""
+  };
+
+  render() {
+    const { email, fullname, username, password } = this.state;
+    return (
+      <SignupForm
+        emailValue={email}
+        fullnameValue={fullname}
+        usernameValue={username}
+        passwordValue={password}
+        handleInputChange={this._handleInputChange}
+        handleSubmit={this._handleSubmit}
+      />
+    );
+  }
+
+  _handleInputChange = event => {
+    const { target: { value, name } } = event;
+    //console.log(value, name);
+    this.setState({
+      [name]: value
+    });
+  };
+
+  _handleSubmit = event => {
+    event.preventDefault();
+    
+    console.log(this.state);
+
+    // redux action be here!
+  };
+}
 
 export default Container;
--- frontend/src/components/SignupForm/presenter.js
+++ frontend/src/components/SignupForm/presenter.js
@@ -4,40 +4,68 @@ import PropTypes from "prop-types";
 import styles from "shared/formStyles.scss";
 
 const SignupForm = (props, context) => (
-    <div className={styles.formComponent}>
-      <h3 className={styles.signupHeader}>
+  <div className={styles.formComponent}>
+    <h3 className={styles.signupHeader}>
       {context.t("Sign up to see photos and videos from your friends.")}
-        
-      </h3>
-      <button className={styles.button}>
-        <Ionicon icon="logo-facebook" fontSize="20px" color="white" /> 
-        
-        {context.t("Log in with Facebook")}
-      </button>
-      <span className={styles.divider}>or</span>
-      <form className={styles.form}>
-        <input type="email" placeholder={context.t("Email")} className={styles.textInput} />
-        <input type="text" placeholder={context.t("Full Name")} className={styles.textInput} />
-        <input
-          type="username"
-          placeholder={context.t("Username")}
-          className={styles.textInput}
-        />
-        <input
-          type="password"
-          placeholder={context.t("Password")}
-          className={styles.textInput}
-        />
-        <input type="submit" value="Sign up" className={styles.button} />
-      </form>
-      <p className={styles.terms}>
-        By signing up, you agree to our <span>Terms & Privacy Policy</span>.
-      </p>
-    </div>
-  );
-  
-  SignupForm.contextTypes = {
-    t: PropTypes.func.isRequired
-  };
-  
-  export default SignupForm;
+    </h3>
+    <button className={styles.button}>
+      <Ionicon icon="logo-facebook" fontSize="20px" color="white" />
+
+      {context.t("Log in with Facebook")}
+    </button>
+    <span className={styles.divider}>or</span>
+    <form className={styles.form} onSubmit={props.handleSubmit}>
+      <input
+        type="email"
+        placeholder={context.t("Email")}
+        className={styles.textInput}
+        value={props.emailValue}
+        onChange={props.handleInputChange}
+        name="email"
+      />
+      <input
+        type="text"
+        placeholder={context.t("Full Name")}
+        className={styles.textInput}
+        value={props.fullnameValue}
+        onChange={props.handleInputChange}
+        name="fullname"
+      />
+      <input
+        type="username"
+        placeholder={context.t("Username")}
+        className={styles.textInput}
+        value={props.usernameValue}
+        onChange={props.handleInputChange}
+        name="username"
+      />
+      <input
+        type="password"
+        placeholder={context.t("Password")}
+        className={styles.textInput}
+        value={props.passwordValue}
+        onChange={props.handleInputChange}
+        name="password"
+      />
+      <input type="submit" value="Sign up" className={styles.button} />
+    </form>
+    <p className={styles.terms}>
+      By signing up, you agree to our <span>Terms & Privacy Policy</span>.
+    </p>
+  </div>
+);
+
+SignupForm.propTypes = {
+  emailValue: PropTypes.string.isRequired,
+  fullnameValue: PropTypes.string.isRequired,
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
+
+SignupForm.contextTypes = {
+  t: PropTypes.func.isRequired
+};
+
+export default SignupForm;
