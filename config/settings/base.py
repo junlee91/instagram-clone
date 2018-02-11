ed1e956fe39b172651a84e4ebfe6a37bd6eb25d6
junlee91 <jlee757@uic.edu>
2018-02-11 01:15:11 -0600

    Requirements for eb

--- config/settings/base.py
+++ config/settings/base.py
@@ -93,7 +93,7 @@ MIGRATION_MODULES = {
 # DEBUG
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool('DJANGO_DEBUG', False)
+DEBUG = env.bool('DJANGO_DEBUG', True)
 
 # FIXTURE CONFIGURATION
 # ------------------------------------------------------------------------------
