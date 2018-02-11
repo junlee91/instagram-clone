259ad5be7202281cdd3773c8d4e6ee7fc21699c9
junlee91 <jlee757@uic.edu>
2018-02-11 01:43:59 -0600

    requirement update

--- config/settings/production.py
+++ config/settings/production.py
@@ -128,7 +128,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
 
 # Use the Heroku-style specification
 # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
-#DATABASES['default'] = env.db('DATABASE_URL')
+DATABASES['default'] = env.db('DATABASE_URL')
 #DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)
 
 # CACHING
